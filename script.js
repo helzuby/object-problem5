fda03bde9227feb7d41dfbac2316f40a9d309072
Nnachi Helen Chidinma <105408064+helzuby@users.noreply.github.com>
2023-03-22 12:06:54 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const books = [ 
+
+   { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+
+   { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+
+   { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+
+   ];
+   function sortObj (a,b) {
+   	if (a.title < b.title){
+   		return -1
+   	}
+   	else if (a.title > b.title){
+   		return 1
+   }
+   else {
+   	return 0;
+   }
+}
+console.log(books.sort(sortObj))
